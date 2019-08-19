28a7374f444d6077410d8e4a8fccaee9ca2c89b5
jeremy <jeremy@aono.fr>
2019-08-19 10:22:37 +0200

    Add poc maps on regions & départements

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+   <MapRegion/>
+    <!--<MapDepartment/>-->
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import MapRegion from './components/MapRegion.vue'
+//import MapDepartment from './components/MapDepartment.vue'
+
+export default {
+  name: 'app',
+  components: {
+    MapRegion,
+    //MapDepartment
+  },
+  methods: {
+
+  },
+  data(){
+    return{
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
